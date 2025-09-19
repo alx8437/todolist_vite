bd97e5bb69b228db4f7f97018039f522fa148022
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-19 16:37:18 +0300

    12. prettier was added

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const token = 'XXX'
-const apiKey = 'XXX'
+const token = '85459685-528d-415c-a12f-0cc8ddead5ff'
+const apiKey = 'e655fc0d-99c3-4c81-8dea-0837243fe8bf'
 
 export const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1',
