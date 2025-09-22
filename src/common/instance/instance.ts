8c7198736445f7015df4d2e5e4f8b83275e5b97c
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-22 10:02:05 +0300

    12. prettier was applied

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -1,12 +1,12 @@
-import axios from 'axios'
+import axios from "axios"
 
-const token = '85459685-528d-415c-a12f-0cc8ddead5ff'
-const apiKey = 'e655fc0d-99c3-4c81-8dea-0837243fe8bf'
+const token = "85459685-528d-415c-a12f-0cc8ddead5ff"
+const apiKey = "e655fc0d-99c3-4c81-8dea-0837243fe8bf"
 
 export const instance = axios.create({
-  baseURL: 'https://social-network.samuraijs.com/api/1.1',
+  baseURL: "https://social-network.samuraijs.com/api/1.1",
   headers: {
     Authorization: `Bearer ${token}`,
-    'API-KEY': apiKey,
+    "API-KEY": apiKey,
   },
 })
