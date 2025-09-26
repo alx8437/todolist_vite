0290977000418340146afa33152358b7a0d8a665
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-26 16:44:01 +0300

    12. use Omit typescript

--- src/features/todolists/api/taskApi.types.ts
+++ src/features/todolists/api/taskApi.types.ts
@@ -19,14 +19,7 @@ export type TaskResponseType = {
   error: string | null
 }
 
-export type UpdateTaskModel = {
-  title: string
-  description: string | null
-  status: TaskStatus
-  priority: TaskPriority
-  startDate: string | null
-  deadline: string | null
-}
+export type UpdateTaskModel = Omit<Task, "id" | "todoListId" | "order" | "addedDate">
 
 export type ChangeTaskPayload = {
   todolistId: string
