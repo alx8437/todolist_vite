087d49a1c94eef4b8a63d25eb96d4ef6072989e5
Aleks Zelenskiy <dezion777@gmail.com>
2025-11-18 15:58:37 +0300

    13. createSlice for todolists reducer

---
+++ src/features/todolists/model/todolists-slice.ts
@@ -0,0 +1,49 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit"
+
+export type Todolist = {
+  id: string
+  title: string
+  filter: FilterValues
+}
+
+export type FilterValues = "all" | "active" | "completed"
+
+const todolistsSlice = createSlice({
+  name: "todolists",
+  initialState: [] as Todolist[],
+  reducers: (create) => ({
+    deleteTodolistAC: create.reducer<{ id: string }>((state, action) => {
+      const index = state.findIndex((todolist) => todolist.id === action.payload.id)
+      if (index !== -1) {
+        state.splice(index, 1)
+      }
+    }),
+    createTodolistAC: create.preparedReducer(
+      (title: string) => {
+        return { payload: { id: nanoid(), title } }
+      },
+      (state, action) => {
+        state.push({ ...action.payload, filter: "all" })
+      },
+    ),
+    changeTodolistTitleAC: create.reducer<{ id: string; title: string }>((state, action) => {
+      const index = state.findIndex((todolist) => todolist.id === action.payload.id)
+
+      if (index !== -1) {
+        state[index].title = action.payload.title
+      }
+    }),
+    changeTodolistFilterAC: create.reducer<{ id: string; filter: FilterValues }>((state, action) => {
+      const todolist = state.find((tl) => tl.id === action.payload.id)
+
+      if (todolist) {
+        todolist.filter = action.payload.filter
+      }
+    }),
+  }),
+})
+
+export const todolistsReducer = todolistsSlice.reducer
+
+export const { createTodolistAC, changeTodolistTitleAC, changeTodolistFilterAC, deleteTodolistAC } =
+  todolistsSlice.actions
