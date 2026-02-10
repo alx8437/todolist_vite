8f57c6ca8658a1e5e77833449515dcea77a3f7d1
Aleks Zelenskiy <dezion777@gmail.com>
2026-02-10 16:42:56 +0300

    16. routing was added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css"
-import { Main } from "@/app/Main"
 import { Header } from "@/common/components/Header/Header"
 import { useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
@@ -7,6 +6,7 @@ import CssBaseline from "@mui/material/CssBaseline"
 import { ThemeProvider } from "@mui/material/styles"
 import { selectThemeMode } from "@/app/app-slice.ts"
 import { ErrorSnackbar } from "@/common/components"
+import { Routing } from "@/common/routing"
 
 export const App = () => {
   const themeMode = useAppSelector(selectThemeMode)
@@ -18,9 +18,9 @@ export const App = () => {
       <div className={"app"}>
         <CssBaseline />
         <Header />
-        <Main />
+        <Routing />
+        <ErrorSnackbar />
       </div>
-      <ErrorSnackbar />
     </ThemeProvider>
   )
 }
