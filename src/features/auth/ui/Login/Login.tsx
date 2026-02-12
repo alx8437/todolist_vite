98723ee3dbd2c95c7f131bcfd49ba4e7a4a7171d
Aleks Zelenskiy <dezion777@gmail.com>
2026-02-12 15:58:44 +0300

    16. react-hook-form basic usage

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -9,11 +9,38 @@ import FormGroup from "@mui/material/FormGroup"
 import FormLabel from "@mui/material/FormLabel"
 import Grid from "@mui/material/Grid2"
 import TextField from "@mui/material/TextField"
+import { Controller, SubmitHandler, useForm } from "react-hook-form"
+import styles from "./Login.module.css"
+import { InputAdornment, InputLabel, OutlinedInput } from "@mui/material"
+import IconButton from "@mui/material/IconButton"
+import { useState } from "react"
+import { Visibility, VisibilityOff } from "@mui/icons-material"
+
+type LoginInputs = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
 
 export const Login = () => {
   const themeMode = useAppSelector(selectThemeMode)
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+  const [showPassword, setShowPassword] = useState(true)
 
   const theme = getTheme(themeMode)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    control,
+  } = useForm<LoginInputs>({ defaultValues: { email: "", password: "", rememberMe: false } })
+
+  const onSubmit: SubmitHandler<LoginInputs> = (data: LoginInputs) => {
+    console.log(data)
+    reset()
+  }
 
   return (
     <Grid container justifyContent={"center"}>
@@ -38,14 +65,56 @@ export const Login = () => {
             <b>Password:</b> free
           </p>
         </FormLabel>
-        <FormGroup>
-          <TextField label="Email" margin="normal" />
-          <TextField type="password" label="Password" margin="normal" />
-          <FormControlLabel label="Remember me" control={<Checkbox />} />
-          <Button type="submit" variant="contained" color="primary">
-            Login
-          </Button>
-        </FormGroup>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormGroup>
+            <TextField
+              label="Email"
+              margin="normal"
+              {...register("email", {
+                pattern: {
+                  value: emailRegex,
+                  message: "Incorrect email address",
+                },
+                required: { value: true, message: "email is required" },
+              })}
+              error={!!errors.email}
+            />
+            {!!errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
+            <FormControl sx={{ marginTop: "10px" }}>
+              <InputLabel htmlFor="password-input">Password</InputLabel>
+              <OutlinedInput
+                id="password-input"
+                label={"Password"}
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                {...register("password", {
+                  required: { value: true, message: "Password is required" },
+                  minLength: { value: 8, message: "Minimum 8 symbols" },
+                })}
+              />
+              {!!errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
+            </FormControl>
+            <FormControlLabel
+              label="Remember me"
+              control={
+                <Controller
+                  name="rememberMe"
+                  control={control}
+                  render={({ field: { value, ...rest } }) => <Checkbox checked={value} {...rest} />}
+                />
+              }
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
+          </FormGroup>
+        </form>
       </FormControl>
     </Grid>
   )
