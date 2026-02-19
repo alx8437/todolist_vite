8f22e061c7e885b9a8c31920394ae10166a36ee5
Aleks Zelenskiy <dezion777@gmail.com>
2026-02-19 16:12:47 +0300

    Go to Main Page button was added

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router"
+import { NavButton } from "@/common/components/NavButton/NavButton"
 import styles from "./PageNotFound.module.css"
 
 export const PageNotFound = () => (
-  <>
+  <div className={styles.container}>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
-  </>
+    <div className={styles.buttonContainer}>
+      <Link to="/">
+        <NavButton>Go to Main Page</NavButton>
+      </Link>
+    </div>
+  </div>
 )
